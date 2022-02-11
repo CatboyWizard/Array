6b1cf77a2ad4ad024034036291f76ecb6ea037b2
CatboyWizard <90338417+CatboyWizard@users.noreply.github.com>
2022-02-11 17:28:42 -0500

    Add files via upload

---
+++ array.js
@@ -0,0 +1,44 @@
+function myFunction() {
+    const numbers =[1, 2, 3, 4];
+
+    numbers.length //4
+    
+    // Accessing an array element
+    const myArray = [100, 200, 300];
+    
+    console.log(myArray[0]); //100
+    console.log(myArray[1]); //200
+    console.log(myArray[2]); //300   
+}
+
+function myFun() {
+ // Adding a single element:
+const cart = ['apple', 'orange'];
+cart.push('pear');
+}
+
+function myNumber() {
+    // Adding multiple element:
+const number =[1, 2];
+number.push(3, 4, 5);
+}
+
+function myFood() {
+    const ingredients = ['eggs', 'flour', 'chocolate'];
+
+const poppedIngredient = ingredients.pop(); //chocolate
+console.log(ingredients); //['eggs', 'flour']
+}
+
+function myFive() {
+    const names = ['Alice', 'Bob'];
+
+names.push('Carl');
+// ['Alice', 'Bob'. 'Carl']
+}
+
+// an array containing numbers
+const numberArray = [0, 1, 2, 3];
+
+// An array containig different data types
+const mixedArray = [1, 'chicked', false];
